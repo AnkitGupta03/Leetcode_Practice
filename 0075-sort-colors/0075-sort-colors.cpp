c14c384d8043aac2cadff35b6fee63a6fa917b98
Ankit Kumar Gupta <96652394+AnkitGupta03@users.noreply.github.com>
2023-08-22 23:15:59 +0530

    Time: 0 ms (100.00%), Space: 8.3 MB (42.26%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,7 +1,18 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        //just kidding, this can't be the solution(lol);
-        sort(nums.begin(), nums.end());
+        //one solution can be merge sort, but that would be O(n*logn), and this is O(2*n) solution.
+        int n = nums.size();
+        int cnt0 = 0; int cnt1 = 0; int cnt2 = 0;
+        for(int i=0; i<n; i++){
+            if(nums[i] == 0) cnt0++;
+            else if(nums[i] == 1) cnt1++;
+            else cnt2++;
+        }
+        
+        for(int i=0; i<cnt0; i++) nums[i] = 0;
+        for(int i=cnt0; i< cnt0 + cnt1; i++) nums[i] = 1;
+        for(int i=cnt0 + cnt1; i<n; i++) nums[i] = 2;
+        
     }
 };
