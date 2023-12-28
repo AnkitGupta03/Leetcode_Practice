7c124aeb94c1e023ea2888e019eb195f1b972ca8
Ankit Kumar Gupta <96652394+AnkitGupta03@users.noreply.github.com>
2023-12-28 18:02:38 +0530

    Time: 487 ms (22.39%), Space: 13.6 MB (65.41%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -10,18 +10,18 @@ public:
         int k = totalsum/2;
         
         vector<vector<bool>> dp(n+1, vector<bool> (k+1));
-        for(int i=0; i<n ;i++){
+        for(int i=0; i<=n ;i++){
             dp[i][0] = 1;
         }
         for(int i=1; i<=k; i++){
             dp[0][i] = 0;
         }
         
-        for(int i=1; i<n; i++){
+        for(int i=1; i<=n; i++){
             for(int j=1; j<=k; j++){
-                dp[i][j] = ((j-nums[i])>=0) ? (dp[i-1][j] || dp[i-1][j-nums[i]]) : (dp[i-1][j]);
+                dp[i][j] = ((j-nums[i-1])>=0) ? (dp[i-1][j] || dp[i-1][j-nums[i-1]]) : (dp[i-1][j]);
             }
         }
-        return dp[n-1][k];
+        return dp[n][k];
     }
 };
