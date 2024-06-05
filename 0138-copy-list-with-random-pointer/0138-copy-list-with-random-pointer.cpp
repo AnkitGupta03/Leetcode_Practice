0bfd363acf3fad3e1e224bc78225fbe1bbefec0d
Ankit Kumar Gupta <96652394+AnkitGupta03@users.noreply.github.com>
2024-06-05 11:55:24 +0530

    Time: 10 ms (22.92%), Space: 12.7 MB (97.70%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
@@ -17,20 +17,33 @@ public:
 class Solution {
 public:
     Node* copyRandomList(Node* head) {
+        //optimal solution without using extra space.
         Node *temp = head;
-        map<Node*, Node*> mpp;
+        //step 1. creating copyNodes and inserting them between the original nodes
         while(temp){
-            Node *newNode = new Node(temp -> val);
-            mpp[temp] = newNode;
-            temp = temp -> next;
+            Node *copyNode = new Node(temp->val);
+            copyNode -> next = temp -> next;
+            temp -> next = copyNode;
+            temp = temp -> next -> next;
         }
+        //step2. attaching random pointers in copyNodes.
         temp = head;
         while(temp){
-            Node *copyNode = mpp[temp];
-            copyNode -> next = mpp[temp -> next];
-            copyNode -> random = mpp[temp -> random];
+            Node *copyNode = temp -> next;
+            if(temp -> random) copyNode -> random = temp -> random -> next;
+            else copyNode -> random = nullptr;
+            temp = temp -> next -> next;
+        }
+        //step 3. attaching next pointers in copyNodes.
+        temp = head;
+        Node *dummyNode = new Node(-1);
+        Node *res = dummyNode;
+        while(temp){
+            res -> next = temp -> next;
+            temp -> next = temp -> next -> next;
+            res = res -> next;
             temp = temp -> next;
         }
-        return mpp[head];
+        return dummyNode -> next;
     }
 };
