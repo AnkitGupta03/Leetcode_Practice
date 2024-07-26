8388cf1c80cf1cb5ed3707a4eec39bbc1e7acecb
Ankit Kumar Gupta <96652394+AnkitGupta03@users.noreply.github.com>
2024-07-26 11:23:44 +0530

    Time: 0 ms (100.00%), Space: 9.8 MB (11.21%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,26 +1,21 @@
 class Solution {
 public:
-    int houserobber(vector<int>& nums){
-        int n = nums.size();
-        if(n == 1) return nums[0];
-        
-        int prev2 = nums[0];
-        int prev1 = max(nums[0], nums[1]);
-        
-        for(int i=2; i<n; i++){
-            int curr = max(prev1, prev2 + nums[i]);
-            prev2 = prev1;
-            prev1 = curr;
-        }
-        return prev1;
+    int helper(vector<int> &nums, int i, vector<int> &dp){
+        if(i==0) return dp[i] = nums[0];
+        if(i==1) return dp[i] = max(nums[0], nums[1]);
+        if(dp[i] != -1) return dp[i];
+        return dp[i] = max(helper(nums, i-1, dp), helper(nums, i-2, dp) + nums[i]);
     }
     int rob(vector<int>& nums) {
         int n = nums.size();
         if(n == 1) return nums[0];
-        if(n == 2) return max(nums[0] , nums[1]);
-        vector<int> v1(nums.begin(), nums.end() - 1);
+        if(n == 2) return max(nums[0], nums[1]);
+        vector<int> dp1(n-1, -1);
+        vector<int> dp2(n-1, -1);
+        
+        vector<int> v1(nums.begin(), nums.end()-1);
         vector<int> v2(nums.begin() + 1, nums.end());
         
-        return max(houserobber(v1), houserobber(v2));
+        return max(helper(v1, n-2, dp1), helper(v2, n-2, dp2));
     }
 };
