5b508ab1794336e91897a2c61edd928c1970d4b7
Ankit Kumar Gupta <96652394+AnkitGupta03@users.noreply.github.com>
2024-07-26 10:48:53 +0530

    Time: 0 ms (100.00%), Space: 9.3 MB (28.16%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,14 +1,18 @@
 class Solution {
 public:
-    int helper(vector<int> &nums, int i, vector<int> &dp){
-        if(i==0) return dp[i] = nums[0];
-        if(i==1) return dp[i] = max(nums[0], nums[1]);
-        if(dp[i] != -1) return dp[i];
-        return dp[i] = max(helper(nums, i-1, dp), helper(nums, i-2, dp) + nums[i]);
-    }
+    //space optimised dp iterative solution.
     int rob(vector<int>& nums) {
         int n = nums.size();
-        vector<int> dp(n, -1);
-        return helper(nums, n-1, dp);
+        if(n == 1) return nums[0];
+        
+        int prev2 = nums[0];
+        int prev1 = max(nums[0], nums[1]);
+        
+        for(int i=2; i<n; i++){
+            int curr = max(prev1, prev2 + nums[i]);
+            prev2 = prev1;
+            prev1 = curr;
+        }
+        return prev1;
     }
 };
