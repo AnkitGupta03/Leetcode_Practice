abb32a1ed4b118fe776e0464015acc0185c9eccf
Ankit Kumar Gupta <96652394+AnkitGupta03@users.noreply.github.com>
2024-08-10 01:13:36 +0530

    Time: 4 ms (31.61%), Space: 9.5 MB (33.36%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
-    //space optimised dp iterative solution.
     int rob(vector<int>& nums) {
         int n = nums.size();
+        vector<int> dp(n, 0);
+        //edge case
         if(n == 1) return nums[0];
-        
-        int prev2 = nums[0];
-        int prev1 = max(nums[0], nums[1]);
-        
+        // base cases.
+        dp[0] = nums[0];
+        dp[1] = max(nums[0], nums[1]);
+        //transition.
         for(int i=2; i<n; i++){
-            int curr = max(prev1, prev2 + nums[i]);
-            prev2 = prev1;
-            prev1 = curr;
+            dp[i] = max(dp[i-1], (dp[i-2] + nums[i]));
         }
-        return prev1;
+        return dp[n-1];
     }
 };
