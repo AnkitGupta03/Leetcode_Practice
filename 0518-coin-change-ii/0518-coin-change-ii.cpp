18d7d7b7097415b19872107056a217c4ec11f236
Ankit Kumar Gupta <96652394+AnkitGupta03@users.noreply.github.com>
2024-08-15 18:59:47 +0530

    Time: 2 ms (93.79%), Space: 8.7 MB (91.94%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,18 +1,14 @@
 class Solution {
 public:
-    int f(int amount, vector<int> &coins, int index, vector<vector<int>> &dp){
-        if(amount == 0) return 1;
-        if(amount < 0) return 0;
-        if(dp[index][amount] != -1) return dp[index][amount];
-        int count = 0;
-        for(int j=index; j<coins.size(); j++){
-            count += f(amount - coins[j], coins, j, dp);
+int change(int amount, vector<int>& coins) {
+    vector<int> dp(amount + 1, 0);
+    // base case
+    dp[0] = 1;
+    for (int j = 0; j < coins.size(); j++) {
+        for (int i = coins[j]; i <= amount; i++) {
+            dp[i] += dp[i - coins[j]];
         }
-        return dp[index][amount] = count;
     }
-    int change(int amount, vector<int>& coins) {
-        int n = coins.size();
-        vector<vector<int>> dp(n, vector<int> (amount + 1, -1));
-        return f(amount, coins, 0, dp);
-    }
+    return dp[amount];
+}
 };
